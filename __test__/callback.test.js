c574431d198d7800801b2f4367463796ead2f316
FernandoSanabria <ing.fernandosanabria@gmail.com>
2021-01-29 10:25:25 -0500

    callbacks

---
+++ __test__/callback.test.js
@@ -0,0 +1,11 @@
+import {callbackHell} from '../callback';
+
+describe('callbacks', () => {
+    test('callback test ', done => {
+        function otherCallback(data) {
+            expect(data).toBe('Hola bebé ')
+            done();
+        }
+        callbackHell(otherCallback);
+    });
+});
---
+++ __test__/setup.test.js
@@ -0,0 +1,11 @@
+afterEach(() => console.log('Despues de cada test'));
+afterAll(() => console.log('Despues de Todo test'));
+
+beforeAll(() => console.log('Antes de todos los test'));
+beforeEach(() => console.log('antes de de cada test'));
+
+describe('veamos', () => {
+   test('Si es o no', () => {
+       expect(true).toBeTruthy();
+   }); 
+});
